71994769f4344df6a920ad3cedc59f1959f7d6fc
Tsunemoriii <134905272+Tsunemoriii@users.noreply.github.com>
2023-12-15 13:37:46 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -105,7 +105,7 @@ async def ForceSub(bot: Client, update: Message, file_id: str = False):
                 InlineKeyboardButton("𝗝𝗼𝗶𝗻 ⚡️⚡️𝗔𝗻𝗶𝗺𝗲 𝗙𝗹𝗶𝘅⚡️⚡️", url='https://t.me/Anime_Flix_Pro')
             ],
             [
-                InlineKeyboardButton("⚡️𝗝𝗼𝗶𝗻 𝗧𝗵𝗲 𝗗𝗮𝗻𝗴𝗲𝗿 𝗜𝗻 𝗠𝘆 𝗛𝗲𝗮𝗿𝘁⚡️", url=invite_link)
+                InlineKeyboardButton("⚡️𝗝𝗼𝗶𝗻 𝗗𝗮𝗿𝗹𝗶𝗻𝗴 𝗜𝗻 𝗧𝗵𝗲 𝗙𝗿𝗮𝗻𝘅𝘅⚡️", url=invite_link)
             ],
             [
                 InlineKeyboardButton(">>> 𝗧𝗿𝘆 𝗔𝗴𝗮𝗶𝗻 <<<", url=file_id)
